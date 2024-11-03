ff79851ba3139a96e0c71ea15be55096e31ba370
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-11-03 15:05:45 +0300

    Update
    updated some vidual elements in the home page

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../index.css'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/notes">Notes</Link></li>
+    <nav className='px-24 py-10 bg-slate-200'>
+      <ul className='flex justify-between items-center'>
+        <li className='text-2xl'><Link to="/">Webtor</Link></li>
+        <ul className="flex justify-center items-center">
+          <li className='mx-3'><Link to="/about">About</Link></li>
+          <li className='mx-3'><Link to="/notes">Notes</Link></li>
+        </ul>
       </ul>
     </nav>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import Img1 from '../assets/img1.jpg';
+import Img2 from '../assets/img2.jpg'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page.</p>
+      <header className='p-10 py-48 bg-slate-500 flex flex-col justify-center items-center text-center'>
+        <div id="cont">
+          <h1 className='text-5xl mb-8'>Webtor</h1>
+          <p>Build your ultimate Frontend experience</p>
+        </div>
+      </header>
+
+      <section className='flex justify-evenly items-center p-10'>
+        <div id="text" className='flex-1'>
+          <h1 className='text-3xl mb-4'>Learn from Zero to Hero</h1>
+          <p>At Webtor, we turn beginners into frontend development heroes. Our course guides you step-by-step, from basic HTML, CSS, and JavaScript to advanced frameworks like React. With hands-on projects and expert mentorship, you'll gain the skills needed to thrive in the tech industry. Join us and transform your passion into professional prowess.</p>
+        </div>
+        <div id="img" className='flex-1 p-4'>
+          <img className='w-full h-auto' src={Img1} alt="Frontend Development" />
+        </div>
+      </section>
+
+      <section className='flex justify-evenly items-center p-10'>
+      <div id="img" className='flex-1 p-4'>
+          <img className='w-full h-auto' src={Img2} alt="Frontend Development" />
+        </div>
+
+        <div id="text" className='flex-1'>
+          <h1 className='text-3xl mb-4'>Learn from Zero to Hero</h1>
+          <p>At Webtor, we turn beginners into frontend development heroes. Our course guides you step-by-step, from basic HTML, CSS, and JavaScript to advanced frameworks like React. With hands-on projects and expert mentorship, you'll gain the skills needed to thrive in the tech industry. Join us and transform your passion into professional prowess.</p>
+        </div>
+        
+      </section>
     </div>
   );
 };
 
 export default Home;
+
